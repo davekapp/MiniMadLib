2ab6d6fb4f163ef4981ce42806d0a187d2dd3692
David Kapp <davekapp@gmail.com>
2012-01-26 17:56:32 -0700

    Initial Commit

---
+++ MiniMadLib/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MiniMadLib
+//
+//  Created by David Kapp on 1/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MiniMadLib/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MiniMadLib
+//
+//  Created by David Kapp on 1/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
