0b7f15bd77d68c39eceb33bca6274bf93a32f8f1
David Kapp <davekapp@gmail.com>
2012-01-26 19:50:38 -0700

    initial commit

--- MiniMadLib/ViewController.h
+++ MiniMadLib/ViewController.h
@@ -8,6 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate, UITextViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UITextField *place;
+@property (strong, nonatomic) IBOutlet UITextField *verb;
+@property (strong, nonatomic) IBOutlet UITextField *number;
+@property (strong, nonatomic) IBOutlet UITextView *storyTemplate;
+@property (strong, nonatomic) IBOutlet UITextView *story;
+@property (strong, nonatomic) IBOutlet UIButton *generateButton;
+
+- (IBAction)generateStory:(id)sender;
+- (IBAction)endEditing:(id)sender;
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField;
+- (BOOL)textViewShouldEndEditing:(UITextView *)textView;
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event;
 
 @end
--- MiniMadLib/ViewController.m
+++ MiniMadLib/ViewController.m
@@ -8,7 +8,20 @@
 
 #import "ViewController.h"
 
+@interface ViewController ()
+  
+@property (strong, nonatomic) NSMutableSet *dissmissableViews;
+
+@end
+  
 @implementation ViewController
+@synthesize story;
+@synthesize generateButton;
+@synthesize place;
+@synthesize verb;
+@synthesize number;
+@synthesize storyTemplate;
+@synthesize dissmissableViews;
 
 - (void)didReceiveMemoryWarning
 {
@@ -20,12 +33,26 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+  [super viewDidLoad];  
 	// Do any additional setup after loading the view, typically from a nib.
+  self.dissmissableViews = [[NSMutableSet alloc] init];
+  
+  [self.dissmissableViews addObject:self.place];
+  [self.dissmissableViews addObject:self.verb];
+  [self.dissmissableViews addObject:self.number];
+  [self.dissmissableViews addObject:self.storyTemplate];
+  [self.dissmissableViews addObject:self.story];
 }
 
 - (void)viewDidUnload
 {
+    [self setPlace:nil];
+    [self setVerb:nil];
+    [self setNumber:nil];    
+    [self setStory:nil];
+    [self setStoryTemplate:nil];
+    [self setGenerateButton:nil];
+    [self setDissmissableViews:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -59,6 +86,41 @@
   } else {
       return YES;
   }
+}
+
+- (IBAction)generateStory:(id)sender {
+}
+
+- (IBAction)endEditing:(id)sender {
+  [sender resignFirstResponder];
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
+  return YES;
+}
+
+- (BOOL)textViewShouldEndEditing:(UITextView *)textView {
+  return YES;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+  UITouch *touch = [[event allTouches] anyObject];
+  UIView *touchedView = [touch view];
+  UIView *firstResponderView = nil;
+  for (UIView *view in [self.view subviews]) {
+    if ([view isFirstResponder]) {
+      firstResponderView = view;
+      break;
+    }
+  }
+  
+  // if the view that is the first responder is dismissable and the view that was touched is not the first responder,
+  // then make the first responder resign that status to make the keyboard go away
+  if ([dissmissableViews containsObject:firstResponderView] && touchedView != firstResponderView) {
+    [firstResponderView resignFirstResponder];
+  }
+         
+  [super touchesBegan:touches withEvent:event];
 }
 
 @end
